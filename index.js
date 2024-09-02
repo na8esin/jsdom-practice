01af245a15e03065c59b946b868a4de88c82abc2
Takayuki Watanabe <watanabe@sikmi.com>
2024-09-02 09:11:58 +0900

    やっぱり最新版のjsdomでもRequestは使えない

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const dom = new JSDOM(`<body>
+  <div id="content"></div>
+  <script>new Request();</script>
+</body>`, { runScripts: "dangerously" });
+
+console.log("hello");
