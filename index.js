ab0b3eff424e73adf39d794c17c3f2f8cddefa19
Takayuki Watanabe <watanabe@sikmi.com>
2024-09-04 09:12:48 +0900

    ファイルリネーム。シンプルなサンプルを追加

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const dom = new JSDOM(`<body>
-  <div id="content"></div>
-  <script>new Request();</script>
-</body>`, { runScripts: "dangerously" });
-
-console.log("hello");
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
